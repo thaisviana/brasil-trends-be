a193a108554ac4f101f6c29a5ecb4e0c3b518b30
thaisviana <thaisnviana+git@gmail.com>
2018-06-13 16:29:00 -0300

    words

--- trendsApi/elections/models/word.py
+++ trendsApi/elections/models/word.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+PERIODS_CHOICES = (('TDY', 'today'),
+                   ('WKK', 'week'),
+                   ('MNT', 'month'),
+                   ('YAR', 'year'),)
+
 
 class Word(models.Model):
     text = models.CharField(max_length=255)
     size = models.PositiveIntegerField(default=1)
+    period = models.CharField(max_length=2, choices=PERIODS_CHOICES, null=False, blank=False)
+    candidate = models.ForeignKey('Candidate', null=True, blank=True, on_delete=models.SET_NULL)
